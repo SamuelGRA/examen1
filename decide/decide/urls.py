a8cc29e6bfb3214859fe7b38996e852a0812fd6a
Samuel Granado Oliva <samuelgranadooliva36@gmail.com>
2026-01-04 13:45:25 +0100

    cambios que hacen que se pueda ejecutar esto

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+#from rest_framework_swagger.views import get_swagger_view
 
 
-schema_view = get_swagger_view(title='Decide API')
+#schema_view = get_swagger_view(title='Decide API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('doc/', schema_view),
+    #path('doc/', schema_view),
     path('gateway/', include('gateway.urls')),
 ]
 
